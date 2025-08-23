0b9e1d0f114a1e523e2458567f4ef6941f7e8d8c
ARNAB9669 <arnabbiswas9669@gmail.com>
2025-08-24 01:36:41 +0530

    updated

--- Js/index.js
+++ Js/index.js
@@ -298,3 +298,132 @@ console.log(ans)
 
 /* Lec___15*/
 
+// addEventListener("DOMContentLoaded",()=>{
+//     const d = document.getElementById("X");
+//     d.addEventListener("click",(el)=>{
+
+//         d.remove()
+//     });
+// });
+
+
+
+/* Lec___16*/
+
+//Async
+
+//SET TIME OUT
+// const el = document.getElementById("x");
+
+// function chenge(){
+//     el.innerHTML = "<h1>I am an proud Indian</h>";
+
+// }
+
+// const b=document.getElementById("X")
+// b.addEventListener("click", ()=>{
+//     setTimeout(chenge, 500);
+// });
+//CLEAR TIME OUT
+//SERT INTERVAL
+// CLEAR INTERVAL
+
+//collor changer random
+/*
+    const start = document.getElementById("start");
+    const stop = document.getElementById("stop");
+    const C = document.getElementById("x")
+    let TI;
+    
+    start.addEventListener("click",(e)=>{
+        e.preventDefault()
+        start.disabled=true;
+        TI = setInterval(() => {
+            const hex = "0123456789ABCDEF"//Random color genaretor
+            let colour = "#"//Random color genaretor
+            for (let i = 0; i < 6; i++) {//Random color genaretor
+                let x = Math.floor(Math.random() * 16)//Random color genaretor
+                colour += hex[x]//Random color genaretor
+            }
+            document.body.style.backgroundColor=`${colour}`;
+        }, 100)
+        C.innerHTML="<h1> I am an proud INDIAN.</h1>"
+    });
+    
+    stop.addEventListener("click",()=>{
+        clearInterval(TI)
+        start.disabled=false;
+        C.innerHTML="<h1>HI! I am arnab.</h1>"
+    });
+*/
+
+
+/* Lec___17*/
+
+// API request and V8 engine
+
+// const Pr = new Promise()//This is how a promise created and base.
+
+// const P = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         console.log("Async task is coimpleted")
+//         resolve()
+//     },1000)
+// });
+// P.then(()=>{
+//     console.log("Promise consummed")
+// })
+
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("Promise part done")
+//         resolve()
+//     }, 2000)
+// }).then(() => {
+//     console.log(".then part is done")
+// })
+
+
+// new Promise ((resolve, reject)=>{
+//     let count=0;
+//     const X = setInterval(()=>{
+//         console.log("Step x is done.")
+//         count += 1;
+//         if(count===5){
+//             clearInterval(X)
+//             resolve()
+//         }
+//     },1000)
+// }).then(()=>{
+//     console.log("Done")
+// })
+
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const data = {
+//             first_name: "Arnab",
+//             last_name: "Biswas",
+//             class: 9,
+//             age: 16
+//         }
+//         resolve(data)
+//         console.log("Data sent")
+//     }, 5000);
+// }).then((student) => {
+//     document.body.style.backgroundColor = "lightblue"
+//     console.log("Data received")
+//     console.log(student)
+//     let card = document.createElement("div")
+//     card.className = "box";
+//     card.innerHTML = `
+//     <h1>${student.first_name} || ${student.last_name} </h1>
+//     <div>
+//         <h1>Class = ${student.class} </h1>
+//         <h1>Age = ${student.age} </h1>
+//     </div>
+//     `
+//     document.body.appendChild(card)
+// })
+
+
+
